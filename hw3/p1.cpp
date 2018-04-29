4f8f3552e56f1e4e40d38fa73722162a4cb7432d
aben20807 <aben20807@gmail.com>
2018-04-29 12:16:09 +0800

    Apply asytle

--- hw3/p1.cpp
+++ hw3/p1.cpp
@@ -13,7 +13,7 @@ bool is_prime(int n, int f)
     if (n < f * f)
         return true;
     else
-        return is_prime(n, f+2);
+        return is_prime(n, f + 2);
 }
 
 int main()
@@ -21,25 +21,25 @@ int main()
     int n;
     while (scanf("%d", &n)) {
         const clock_t t1 = clock();
-        for (int i = 1; i < n; i+=2){
-            for (int j = 1; j <= i; j+=2){
-                if (n == i+j && is_prime(i, 3) && is_prime(j, 3)){
+        for (int i = 1; i < n; i += 2) {
+            for (int j = 1; j <= i; j += 2) {
+                if (n == i + j && is_prime(i, 3) && is_prime(j, 3)) {
                     printf("%d %d\n", i, j);
                     goto out1;
                 }
             }
         }
-    out1:
+out1:
         printf("%.2lf (s)\n\n", float(clock() - t1) / CLOCKS_PER_SEC);
         const clock_t t2 = clock();
-        for (int i = 1; i < n; i+=2){
+        for (int i = 1; i < n; i += 2) {
             int j = n - i;
-            if (is_prime(i, 3) && is_prime(j, 3)){
+            if (is_prime(i, 3) && is_prime(j, 3)) {
                 printf("%d %d\n", i, j);
                 goto out2;
             }
         }
-    out2:
+out2:
         printf("%.2lf (s)\n\n", float(clock() - t2) / CLOCKS_PER_SEC);
     }
     return 0;
