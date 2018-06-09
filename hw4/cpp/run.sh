2e3a6ccf244680e4f9cbee674afb9813b46c7a4d
aben20807 <aben20807@gmail.com>
2018-06-09 23:51:41 +0800

    Add run bash script for cpp

---
+++ hw4/cpp/run.sh
@@ -0,0 +1,32 @@
+rm omptest.exe
+g++ -g -Wall -fopenmp -o omptest omptest.cpp
+./omptest < ../matrix_mul/test_case/t10.in
+./omptest < ../matrix_mul/test_case/t20.in
+./omptest < ../matrix_mul/test_case/t30.in
+./omptest < ../matrix_mul/test_case/t40.in
+./omptest < ../matrix_mul/test_case/t50.in
+./omptest < ../matrix_mul/test_case/t60.in
+./omptest < ../matrix_mul/test_case/t70.in
+./omptest < ../matrix_mul/test_case/t80.in
+./omptest < ../matrix_mul/test_case/t90.in
+./omptest < ../matrix_mul/test_case/t100.in
+./omptest < ../matrix_mul/test_case/t110.in
+./omptest < ../matrix_mul/test_case/t120.in
+./omptest < ../matrix_mul/test_case/t130.in
+./omptest < ../matrix_mul/test_case/t140.in
+./omptest < ../matrix_mul/test_case/t150.in
+./omptest < ../matrix_mul/test_case/t160.in
+./omptest < ../matrix_mul/test_case/t170.in
+./omptest < ../matrix_mul/test_case/t180.in
+./omptest < ../matrix_mul/test_case/t190.in
+./omptest < ../matrix_mul/test_case/t200.in
+./omptest < ../matrix_mul/test_case/t210.in
+./omptest < ../matrix_mul/test_case/t220.in
+./omptest < ../matrix_mul/test_case/t230.in
+./omptest < ../matrix_mul/test_case/t240.in
+./omptest < ../matrix_mul/test_case/t250.in
+./omptest < ../matrix_mul/test_case/t260.in
+./omptest < ../matrix_mul/test_case/t270.in
+./omptest < ../matrix_mul/test_case/t280.in
+./omptest < ../matrix_mul/test_case/t290.in
+./omptest < ../matrix_mul/test_case/t300.in
